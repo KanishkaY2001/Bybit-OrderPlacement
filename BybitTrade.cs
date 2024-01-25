06b0c9cbda89e81666d782db5c5248614af49add
Kanishka G Yamani <kanishkay2001@gmail.com>
2024-01-25 16:10:16 +1100

    fully endpoint
    New BybitTrade
    - use endpoint

---
+++ BybitTrade.cs
@@ -0,0 +1,205 @@
+using System.Text;
+using Newtonsoft.Json;
+using System.Security.Cryptography;
+
+namespace TradingBot
+{
+    class Settings
+    {
+        public string category { get; set; }
+        public string orderType { get; set; }
+        public string timeInForce { get; set; }
+        public string net { get; set; }
+        public Settings(string _net)
+        {
+            category = "linear";
+            orderType = "market";
+            timeInForce = "GTC";
+            net = _net;
+        }
+    }
+
+    class BybitTime
+    {
+        public long time { get; set; }
+    }
+
+    class BybitHttp
+    {
+        public HttpClient client { get; set; }
+        private Dictionary<string, string> networks { get; set; }
+        private string apiKey { get; set; }
+        private string apiSecret { get; set; }
+        private string recvWindow { get; set; }
+
+        public BybitHttp(string key, string secret)
+        {
+            client = new HttpClient();
+            apiKey = key;
+            apiSecret = secret;
+            recvWindow = "5000";
+            networks = new Dictionary<string, string>
+            {
+                {"testnet", "https://api-testnet.bybit.com"},
+                {"mainnet", "https://api.bybit.com"}
+            };
+        }
+
+        public async Task<string> GetTimestamp()
+        {
+            string timeUri = "https://api-testnet.bybit.com/v5/market/time";
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync(timeUri);
+                response.EnsureSuccessStatusCode();
+                string json = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<BybitTime>(json!.ToString())!.time.ToString();
+            }
+            catch (HttpRequestException e)
+            {
+                return e.ToString();
+            }
+        }
+
+        private string GeneratePostSignature(Dictionary<string, object> parameters, string _timeStamp)
+        {
+            string jsonPayload = JsonConvert.SerializeObject(parameters);
+            string rawData = _timeStamp + apiKey + recvWindow + jsonPayload;
+            using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecret));
+            var signature = hmac.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+            return BitConverter.ToString(signature).Replace("-", "").ToLower();
+        }
+
+        private string GenerateGetSignature(Dictionary<string, object> parameters, string _timeStamp)
+        {
+            string queryString = GenerateQueryString(parameters);
+            string rawData = _timeStamp + apiKey + recvWindow + queryString;
+
+            return ComputeSignature(rawData);
+        }
+
+        private string ComputeSignature(string data)
+        {
+            using var hmac = new HMACSHA256(Encoding.UTF8.GetBytes(apiSecret));
+            byte[] signature = hmac.ComputeHash(Encoding.UTF8.GetBytes(data));
+            return BitConverter.ToString(signature).Replace("-", "").ToLower();
+        }
+
+        private string GenerateQueryString(Dictionary<string, object> parameters)
+        {
+            return string.Join("&", parameters.Select(p => $"{p.Key}={p.Value}"));
+        }
+
+        public async Task<string> SEND(HttpMethod method, string requestUri, Dictionary<string, object> parameters, Settings settings)
+        {
+            string net = networks[settings.net];
+            string timestamp = await GetTimestamp();
+            string jsonPayload = JsonConvert.SerializeObject(parameters);
+            string queryString = method == HttpMethod.Post ? "" : GenerateQueryString(parameters);
+            string signature = method == HttpMethod.Post ? GeneratePostSignature(parameters, timestamp)
+                                                         : GenerateGetSignature(parameters, timestamp);
+            string finalUri = method == HttpMethod.Post ? $"{net}{requestUri}" : $"{net}{requestUri}?{queryString}";
+            HttpRequestMessage request = new(method, finalUri)
+            {
+                Content = method == HttpMethod.Post ? new StringContent(jsonPayload, Encoding.UTF8, "application/json") : null
+            };
+            request.Headers.Add("X-BAPI-API-KEY", apiKey);
+            request.Headers.Add("X-BAPI-SIGN", signature);
+            request.Headers.Add("X-BAPI-SIGN-TYPE", "2");
+            request.Headers.Add("X-BAPI-TIMESTAMP", timestamp);
+            request.Headers.Add("X-BAPI-RECV-WINDOW", recvWindow);
+
+            try
+            {
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+                string responseJson = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(responseJson);
+                return responseJson;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e);
+                return e.ToString();
+            }
+        }
+    }
+
+    class BybitTrade
+    {   
+        private BybitHttp http { get; set; }
+        public Settings settings { get; set; }
+        
+        public BybitTrade(string key, string secret, string net)
+        {
+            settings = new Settings(net);
+            http = new BybitHttp(key, secret);
+        }
+
+        private async Task<bool> ProcessHttp(HttpMethod method, Dictionary<string, object> parameters, string requestUri)
+        {
+            return await http.SEND(method, requestUri, parameters, settings) != string.Empty ? true : false;
+        }
+
+        public async Task<bool> PlaceOrder(string symbol, string quantity, string side)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"category", settings.category},
+                {"symbol", symbol},
+                {"isLeverage", 1},
+                {"side", side},
+                {"orderType", settings.orderType},
+                {"qty", quantity},
+                {"timeInForce", settings.timeInForce},
+            };
+            return await ProcessHttp(HttpMethod.Post, parameters, "/v5/order/create");
+        }
+
+        public async Task<bool> BuyOrder(string symbol, string quantity)
+        {
+            return await PlaceOrder(symbol, quantity, "Buy");
+        }
+
+        public async Task<bool> SellOrder(string symbol, string quantity)
+        {
+            return await PlaceOrder(symbol, quantity, "Sell");
+        }
+
+        public async Task<bool> Position(string symbol)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"category", settings.category},
+                {"symbol", symbol}
+            };
+            return await ProcessHttp(HttpMethod.Get, parameters, "/v5/position/list");
+        }
+
+        public async Task<bool> SetLeverage(string symbol, string buyLeverage, string sellLeverage)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"category", settings.category},
+                {"symbol", symbol},
+                {"buyLeverage", buyLeverage},
+                {"sellLeverage", sellLeverage}
+            };
+            return await ProcessHttp(HttpMethod.Post, parameters, "/v5/position/set-leverage");
+        }
+
+        public async Task<bool> SetTradingStop(string symbol, string takeProfit, string stopLoss)
+        {
+            var parameters = new Dictionary<string, object>
+            {
+                {"category", settings.category},
+                {"symbol", symbol},
+                {"takeProfit", takeProfit},
+                {"stopLoss", stopLoss},
+                {"positionIdx", 0}
+            };
+            //============== INCOMPLETE
+            return await ProcessHttp(HttpMethod.Post, parameters, "/v5/position/trading-stop");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,159 +1,43 @@
-﻿using Newtonsoft.Json;
-
-using bybit.net.api.ApiServiceImp;
-using bybit.net.api.Models.Trade;
-using bybit.net.api.Models;
-using bybit.net.api;
-
-namespace TradingBot
+﻿namespace TradingBot
 {
-    class BybitSystem
-    {
-        public Settings settings { get; set; }
-        public Dictionary<string, OrderInfo> orderDict { get; set; }
-        private string apiKey { get; set; }
-        private string apiSecret { get; set; }
-        BybitTradeService tradeService { get; set; }
-
-        public BybitSystem(Settings _settings, string key, string secret)
-        {
-            settings = _settings;
-            orderDict = new Dictionary<string, OrderInfo>();
-            apiKey = key;
-            apiSecret = secret;
-            tradeService = new(apiKey: apiKey, apiSecret: apiSecret, url: BybitConstants.HTTP_TESTNET_URL);
-        }
-
-        private void AddOrder(string id, string symbol, Side side, string qty)
-        {
-            orderDict.Add
-            (
-                id,
-                new OrderInfo
-                (
-                    settings.category,
-                    symbol,
-                    side,
-                    settings.orderType,
-                    qty,
-                    settings.timeInForce
-                )
-            );
-        }
-
-        public async Task<bool> PlaceOrder(string symbol, string qty)
-        {
-            var side = Side.BUY;
-            var orderObject = await tradeService.PlaceOrder
-            (
-                category: Category.LINEAR, 
-                symbol: symbol, 
-                side: side,
-                orderType: OrderType.MARKET, 
-                qty: qty, 
-                timeInForce: TimeInForce.GTC
-            );
-
-            string id = JsonConvert.DeserializeObject<BybitResponse>(orderObject!.ToString())!.result.orderId;
-            Console.WriteLine($"\nOrder Placed ({DateTime.Now})\nSymbol: {symbol}\nQuantity: {qty}\nOrderId: {id}");
-            AddOrder(id, symbol, side, qty);
-
-            return true;
-        }
-
-        public async Task<bool> CloseOrder(string symbol, string qty)
-        {
-            var side = Side.SELL;
-            var orderObject = await tradeService.PlaceOrder
-            (
-                category: Category.LINEAR, 
-                symbol: symbol, 
-                side: side,
-                orderType: OrderType.MARKET, 
-                qty: qty, 
-                timeInForce: TimeInForce.GTC
-            );
-
-            string id = JsonConvert.DeserializeObject<BybitResponse>(orderObject!.ToString())!.result.orderId;
-            Console.WriteLine($"\nOrder Closed ({DateTime.Now})\nSymbol: {symbol}\nQuantity: {qty}\nOrderId: {id}");
-            AddOrder(id, symbol, side, qty);
-
-            return true;
-        }
-
-        public async Task<bool> CancelOrder(string id)
-        {
-            OrderInfo orderInfo = orderDict[id];
-            Category category = orderInfo.category;
-            string symbol = orderInfo.symbol;
-
-            var orderObject = await tradeService.CancelOrder
-            (
-                orderId: id,
-                category: category, 
-                symbol: symbol
-            );
-
-            Console.WriteLine(orderObject);
-            Console.WriteLine($"\nOrder Cancelled ({DateTime.Now})\nSymbol: {symbol}\nOrderId: {id}");
-            
-            return true;
-        }
-
-        public async Task<bool> PlaceStopLossOrder(string symbol, string qty, string slPrice)
-        {
-            var side = Side.BUY;
-            var orderObject = await tradeService.PlaceOrder
-            (
-                category: Category.LINEAR, 
-                symbol: symbol, 
-                side: side,
-                orderType: OrderType.MARKET, 
-                qty: qty, 
-                stopLoss: slPrice,
-                timeInForce: TimeInForce.GTC
-            );
-
-            string id = JsonConvert.DeserializeObject<BybitResponse>(orderObject!.ToString())!.result.orderId;
-            Console.WriteLine($"\nSL Order Placed ({DateTime.Now})\nSymbol: {symbol}\nQuantity: {qty}\nStopLoss: {slPrice}\nOrderId: {id}");
-            AddOrder(id, symbol, side, qty);
-
-            return true;
-        }
-
-        /**
-        public async Task<bool> UpdateStopLossOrder(string id)
-        {
-            // Not possible with API
-            return true;
-        }
-        **/
-    }
-
-    class Settings
-    {
-        public Category category { get; set; }
-        public OrderType orderType { get; set; }
-        public TimeInForce timeInForce { get; set; }
-        public Settings()
-        {
-            category = Category.LINEAR;
-            orderType = OrderType.MARKET;
-            timeInForce = TimeInForce.GTC;
-        }
-    }
-    
     class System
     {
         public static async Task Main(string[] args)
         {
-            // Configure default and API settings
-            var settings = new Settings();
-            string[] lines = File.ReadAllLines("apiConfig");
-            var sys = new BybitSystem(settings, lines[0], lines[1]);
+            var bybitTrade = new BybitTrade
+            (
+                "1JTREInfQx0zQ5ARwI", // ApiKey
+                "WVDEWELawuoVRRCwQBdfSOZCU9k8O4PBv3VB", // ApiSecret
+                "testnet" // "testnet" or "mainnet"
+            );
 
-            // Commence trading
-            await sys.PlaceStopLossOrder("BLZUSDT", "10", "0.25");
+            // Place "buy" order
+            // Purchase 50 units of BLZUSDT
+            await bybitTrade.BuyOrder("BLZUSDT", "50");
+
+            // Place "sell" order
+            // Sell 8 units of BLZUSDT
+            await bybitTrade.SellOrder("BLZUSDT", "27");
+
+            // Check position
+            // Check current position for BLZUSDT
+            await bybitTrade.Position("BLZUSDT");
+
+            // Set Leverage
+            // For the current 27 units of BLZUSDT, SET the buy leverage to 6 and sell leverage to 4
+            await bybitTrade.SetLeverage("BLZUSDT", "6", "4");
+
+            // Update Leverage
+            // For the current 27 units of BLZUSDT, UPDATE the buy leverage to 6 and sell leverage to 4
+            await bybitTrade.SetLeverage("BLZUSDT", "16", "14");
+
+            // Set trading stop
+            // For the current 27 units of BLZUSDT, set TP at 5, and SL at 0
+            await bybitTrade.SetTradingStop("BLZUSDT", "5", "0");
+
+            // Update trading stop
+            // For the current 27 units of BLZUSDT, UPDATE the tp to 10, and SL to 0.1
+            await bybitTrade.SetTradingStop("BLZUSDT", "10", "0.1");
         }
     }
 }
